0454224f78f6fabe44f8e63d55b4fb9a7ce2a946
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2022-01-17 19:35:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
+aws s3 cp s3://java-artifacts-devops4solutions/sample-demo-java/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /opt/tomcat/apache-tomcat-9.0.56/webapps/LoginWebApp-1.war
-sudo /etc/init.d/tomcat start
+sudo /etc/init.d/tomcat restart
